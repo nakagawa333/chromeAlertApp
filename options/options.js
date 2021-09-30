b1b63c176af17ad2a1b26d3fbd04985277f5012c
nakagawa331 <Nakagawasousei333>
2021-10-01 01:57:35 +0900

    Rate Pitchのrang処理

--- options/options.js
+++ options/options.js
@@ -10,6 +10,16 @@ window.onload = (e) => {
         }
     })
 
+    document.getElementById("rate_range").addEventListener("change",(event) => {
+        const rateValEle = document.getElementById("rate_value")
+        rateValEle.textContent = event.target.value
+    })
+
+    document.getElementById("pitch_range").addEventListener("change",(event) => {
+        const rateValEle = document.getElementById("pitch_value")
+        rateValEle.textContent = event.target.value
+    })
+
     voiceSelect.addEventListener("change",(event) => {
         const voiceName = event.target.value
         const ttsOptions = {
